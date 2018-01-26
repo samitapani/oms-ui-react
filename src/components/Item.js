b380aa33411dbc1655f5f3a6402a59ccea0e2a66
st6 <samitapani@gmail.com>
2018-01-26 13:08:24 +0200

    Add reactiveness to item sums - Add FontAwesome icons

---
+++ src/components/Item.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import plus from '@fortawesome/fontawesome-free-solid/faPlus'
+import minus from '@fortawesome/fontawesome-free-solid/faMinus'
+import euro from '@fortawesome/fontawesome-free-solid/faEuroSign'
+
+export default class Item extends Component {
+
+  constructor(props) {
+    super(props)
+    let count = props.item.count;
+    this.state = {
+      count: count
+    }
+
+    this.countChange = this.countChange.bind(this)
+    this.incCount = this.incCount.bind(this)
+    this.decCount = this.decCount.bind(this)
+  }
+
+  countChange(event) {
+    this.setState({count: parseInt(event.target.value)})
+  }
+
+  incCount(event) {
+    const count = parseInt(this.state.count) + 1
+    this.setState({count: count})
+  }
+
+  decCount(event) {
+    const count = parseInt(this.state.count) - 1
+    this.setState({count: count})
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <span>{this.props.item.product.name}</span>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text"
+                    onClick={this.decCount}
+              >
+                <FontAwesomeIcon icon={minus}/>
+              </span>
+            </div>
+            <input type="number" className="form-control"
+                   value={this.state.count}
+                   onChange={this.countChange}/>
+            <div className="input-group-text">
+
+              <div className="button"
+                   onClick={this.incCount}
+              >
+                <FontAwesomeIcon icon={plus}/>
+              </div>
+            </div>
+
+            <input type="text"
+                   className="form-control "
+                   value={this.props.item.product.price}
+                   disabled/>
+            <div className="input-group-append">
+              <span className="input-group-text"><FontAwesomeIcon icon={euro}/></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
--- src/components/OrderItemList.js
+++ src/components/OrderItemList.js
@@ -1,44 +1,16 @@
 import React, {Component} from 'react';
+import Item from './Item.js';
 
 export default class OrderItemList extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      count: props.count
-    }
-
-    this.handleCountChange = this.handleCountChange.bind(this)
   }
-
-  handleCountChange(event) {
-    console.log(event.target.value)
-    this.setState({count: event.target.value})
-  }
-
   render() {
     const items = (
       <div>
-        {this.props.items.map((order) =>
-          <div key={order.id} className="row">
-            <h4>{order.product.name}</h4>
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text">-</span>
-              </div>
-              <input type="number" className="form-control" value={order.count}
-                     onChange={this.handleCountChange}/>
-              <div className="input-group-append">
-                <span className="input-group-text">+</span>
-              </div>
-
-              <input type="text" className="form-control" value={order.product.price} disabled/>
-              <div className="input-group-append">
-                <span className="input-group-text">€</span>
-              </div>
-            </div>
-            {/*row*/}
-          </div>
+        {this.props.items.map((item) =>
+          <Item key={item.id} item={item}/>
         )}
       </div>
     )
