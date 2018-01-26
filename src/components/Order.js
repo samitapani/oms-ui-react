750d7c68e35bc4c6e462404cb383eae4b612b6ae
st6 <samitapani@gmail.com>
2018-01-26 13:50:59 +0200

    Rename OrderItemList into ItemList - Fix column sizes when two rows visible

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
-import OrderItemList from './OrderItemList.js'
+import ItemList from './ItemList.js'
 import logo from '../logo.svg';
 
 export default class Order extends Component {
   render() {
     const date = new Date(this.props.order.creation).toString()
     return (
-      <div className='col-sm-6 py-4'>
+      <div className='col-sm-12 col-md-6  col-lg-4 py-2'>
         <div className='card p-4'>
           <img className='card-img-top' src={logo} alt={this.props.order.name}/>
           <div className='card-block'>
+
             <h4 className='card-title'>{this.props.order.name}</h4>
-            <div className="card-text">
-              <OrderItemList key={this.props.order.orderItemList.id} items={this.props.order.orderItemList}/>
-            </div>
+
+            <ItemList key={this.props.order.orderItemList.id} items={this.props.order.orderItemList}/>
+
             <div className='card-footer'>
               <small className='text-muted'> {date} </small>
             </div>
