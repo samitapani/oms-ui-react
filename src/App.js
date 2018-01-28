3e86a60720f6555c08c6d81add99118535b896f9
st6 <samitapani@gmail.com>
2018-01-28 23:39:25 +0200

    Implement adding items

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Order from './components/Order.js'
 import axios from 'axios';
 
+const orderData = require('./static/sample-data')
+
 class App extends Component {
   constructor(props) {
     super(props)
-
     // model of state from server response
     this.state = {
       orders: [
@@ -20,13 +21,18 @@ class App extends Component {
             }
           ]
         }
+      ],
+      allProducts: [
+        {
+          id: "", name: "", price: ""
+        }
       ]
     }
   }
 
   render() {
     let orders = []
-    this.state.orders.forEach(x => orders.push(<Order key={x.id} order={x}/>))
+    this.state.orders.forEach(x => orders.push(<Order key={x.id} order={x} allProducts={this.state.allProducts}/>))
 
     return (
       <div className='container'>
@@ -38,199 +44,23 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    //this.fetchData()
-
-    const sampleData = {
-      "content": [
+    const allProducts = [
       {
         "id": 1,
-        "name": "Fishing set.",
-        "orderItemList": [
-          {
-            "id": 1,
-            "count": 2,
-            "product": {
-              "id": 1,
-              "name": "Onki",
-              "price": 99.99
-            }
-          },
-          {
-            "id": 2,
-            "count": 2,
-            "product": {
-              "id": 2,
-              "name": "Kela",
-              "price": 29.9
-            }
-          },
-          {
-            "id": 3,
-            "count": 50,
-            "product": {
-              "id": 3,
-              "name": "Mato",
-              "price": 0.29
-            }
-          }
-        ],
-        "creation": 1516956955941
-      },
-      {
+        "name": "Onki",
+        "price": 99.99
+      }, {
         "id": 2,
-        "name": "More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 4,
-            "count": 50,
-            "product": {
-              "id": 3,
-              "name": "Mato",
-              "price": 0.29
-            }
-          }
-        ],
-        "creation": 1516956956026
-      },
-      {
+        "name": "Kela",
+        "price": 29.9
+      }, {
         "id": 3,
-        "name": "0: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 5,
-            "count": 0,
-            "product": {
-              "id": 4,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956047
-      },
-      {
-        "id": 4,
-        "name": "1: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 6,
-            "count": 1,
-            "product": {
-              "id": 5,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956064
-      },
-      {
-        "id": 5,
-        "name": "2: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 7,
-            "count": 2,
-            "product": {
-              "id": 6,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956088
-      },
-      {
-        "id": 6,
-        "name": "3: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 8,
-            "count": 3,
-            "product": {
-              "id": 7,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956097
-      },
-      {
-        "id": 7,
-        "name": "4: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 9,
-            "count": 4,
-            "product": {
-              "id": 8,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956104
-      },
-      {
-        "id": 8,
-        "name": "5: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 10,
-            "count": 5,
-            "product": {
-              "id": 9,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956119
-      },
-      {
-        "id": 9,
-        "name": "6: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 11,
-            "count": 6,
-            "product": {
-              "id": 10,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956126
-      },
-      {
-        "id": 10,
-        "name": "7: More worms, for Fishing set.",
-        "orderItemList": [
-          {
-            "id": 12,
-            "count": 7,
-            "product": {
-              "id": 11,
-              "name": "1LISA",
-              "price": 1.29
-            }
-          }
-        ],
-        "creation": 1516956956130
+        "name": "Matoja",
+        "price": 0.29
       }
-    ],
-      "last": false,
-      "totalPages": 11,
-      "totalElements": 102,
-      "sort": null,
-      "first": true,
-      "numberOfElements": 10,
-      "size": 10,
-      "number": 0
-    }
-    this.setState({orders: sampleData.content})
+    ]
+
+    this.setState({orders: orderData.content, allProducts: allProducts})
   }
 
   async fetchData() {
---
+++ src/static/sample-data.js
@@ -0,0 +1,192 @@
+const sampleData = {
+  "content": [
+    {
+      "id": 1,
+      "name": "Fishing set.",
+      "orderItemList": [
+        {
+          "id": 1,
+          "count": 2,
+          "product": {
+            "id": 1,
+            "name": "Onki",
+            "price": 99.99
+          }
+        },
+        {
+          "id": 2,
+          "count": 2,
+          "product": {
+            "id": 2,
+            "name": "Kela",
+            "price": 29.9
+          }
+        },
+        {
+          "id": 3,
+          "count": 50,
+          "product": {
+            "id": 3,
+            "name": "Mato",
+            "price": 0.29
+          }
+        }
+      ],
+      "creation": 1516956955941
+    },
+    {
+      "id": 2,
+      "name": "More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 4,
+          "count": 50,
+          "product": {
+            "id": 3,
+            "name": "Mato",
+            "price": 0.29
+          }
+        }
+      ],
+      "creation": 1516956956026
+    },
+    {
+      "id": 3,
+      "name": "0: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 5,
+          "count": 0,
+          "product": {
+            "id": 4,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956047
+    },
+    {
+      "id": 4,
+      "name": "1: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 6,
+          "count": 1,
+          "product": {
+            "id": 5,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956064
+    },
+    {
+      "id": 5,
+      "name": "2: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 7,
+          "count": 2,
+          "product": {
+            "id": 6,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956088
+    },
+    {
+      "id": 6,
+      "name": "3: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 8,
+          "count": 3,
+          "product": {
+            "id": 7,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956097
+    },
+    {
+      "id": 7,
+      "name": "4: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 9,
+          "count": 4,
+          "product": {
+            "id": 8,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956104
+    },
+    {
+      "id": 8,
+      "name": "5: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 10,
+          "count": 5,
+          "product": {
+            "id": 9,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956119
+    },
+    {
+      "id": 9,
+      "name": "6: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 11,
+          "count": 6,
+          "product": {
+            "id": 10,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956126
+    },
+    {
+      "id": 10,
+      "name": "7: More worms, for Fishing set.",
+      "orderItemList": [
+        {
+          "id": 12,
+          "count": 7,
+          "product": {
+            "id": 11,
+            "name": "1LISA",
+            "price": 1.29
+          }
+        }
+      ],
+      "creation": 1516956956130
+    }
+  ],
+  "last": false,
+  "totalPages": 11,
+  "totalElements": 102,
+  "sort": null,
+  "first": true,
+  "numberOfElements": 10,
+  "size": 10,
+  "number": 0
+}
+
+module.exports = sampleData;
